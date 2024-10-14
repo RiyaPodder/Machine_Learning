36d1d46ae85b7cd90ab88fa63aef0cbd7fd9d488
RiyaPodder <31432269+RiyaPodder@users.noreply.github.com>
2024-10-14 12:48:46 +0530

    Add files via upload

---
+++ Principal_Component_Analysis/pca.py
@@ -0,0 +1,74 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def pca(X, D):
+    """
+    PCA
+    Input:
+        X - An (1024, 1024) numpy array
+        D - An int less than 1024. The target dimension
+    Output:
+        X - A compressed (1024, 1024) numpy array
+    """
+
+    #compute covariance matrix of X
+    #cov_mat_X = np.cov(X, rowvar = False)
+
+    #compute SVD
+    #u,s,v = np.linalg.svd(cov_mat_X)
+    u,s,v = np.linalg.svd(X)
+
+    #pick eigen vectors with the largest D eigen values
+    v_new = v[0:D]
+    v_new_transpose = np.transpose(v_new)
+
+    #project data on to the principal components obtained above
+    X_new = np.matmul(X, v_new_transpose)
+
+    #reconstruct compressed image
+    recon_X = np.matmul(X_new, v_new)
+
+    return recon_X
+    #raise NotImplementedError
+
+
+def sklearn_pca(X, D):
+    """
+    Your PCA implementation should be equivalent to this function.
+    Do not use this function in your implementation!
+    """
+    from sklearn.decomposition import PCA
+    p = PCA(n_components=D, svd_solver='full')
+    trans_pca = p.fit_transform(X)
+    X = p.inverse_transform(trans_pca)
+    return X
+
+
+
+
+if __name__ == '__main__':
+    D = 256
+
+    a = Image.open('data/20180108_171224.jpg').convert('RGB')
+    ax1 = plt.subplot(1, 2, 1)
+    ax1.set_title('Original')
+    ax1.imshow(a)
+    b = np.array(a)
+    c = b.astype('float') / 255.
+    for i in range(3):
+        x = c[:, :, i]
+        mu = np.mean(x)
+        x = x - mu
+        x_true = sklearn_pca(x, D)
+        x = pca(x, D)
+        assert np.allclose(x, x_true, atol=0.05)  # Test your results
+        x = x + mu
+        c[:, :, i] = x
+
+    b = np.uint8(c * 255.)
+    a = Image.fromarray(b)
+    ax2 = plt.subplot(1, 2, 2)
+    ax2.set_title('Compressed')
+    ax2.imshow(a)
+    plt.show()
